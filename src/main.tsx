454404300b258ee30ad17f401f21889b4f128f11
Rodrigo S <rodrigojsr@upc.edu.pe>
2026-03-20 10:49:25 -0500

    docs: :memo: se eliminó la línea de documentación del query client

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,6 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import App from './App.tsx'
 import './index.css'
 
-// Inicializamos el QueryClient
 const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
