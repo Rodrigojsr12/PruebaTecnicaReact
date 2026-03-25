f010bb5459b171e516ec4c0b38aa65935bfc1472
Rodrigo S <rodrigojsr@upc.edu.pe>
2026-03-25 09:45:14 -0500

    style: :art: se agregó la paleta de colores de Rick y Morty a la configuración de Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,20 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'brand-brown': '#44281d',
+        'brand-peach': '#e4a788',
+        'brand-yellow': '#f0e14a',
+        'brand-lime': '#97ce4c',
+        'brand-pink': '#e89ac7',
+        'kiwi-green': '#88c23b',
+        'texas-yellow': '#ebc480',
+        'tardis-blue': '#043c6c',
+        'calla-green': '#6b7132',
+        'mystery-teal': '#a6cccc',
+      }
+    },
   },
   plugins: [],
 }
